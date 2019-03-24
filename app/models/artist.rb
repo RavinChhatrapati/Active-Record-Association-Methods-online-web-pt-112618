694cc900cae77cadba809c5f8369e0cd16a00204
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-03-24 01:54:00 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -11,6 +11,6 @@ class Artist < ActiveRecord::Base
   end
 
   def genre_count
-    self.genres.count#return the number of genres associated with the artist
+    self.genre.count#return the number of genres associated with the artist
   end
 end
