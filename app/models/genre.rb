9db6e4487fc0d4aab860071966b29c6b0535e739
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-03-24 02:03:41 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,7 +11,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-  self.artists.collec do |t|
+  self.artists.collect do |t|
     t.name  # return an array of strings containing every musician's name
   end
 end
